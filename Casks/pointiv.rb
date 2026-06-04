d978783bfb299de7fa4db8a2795d948c902ac6c0
Arnav Dewan <57629464+arnxv0@users.noreply.github.com>
2026-06-04 11:48:36 -0700

    Update Pointiv cask to 0.1.10

--- Casks/pointiv.rb
+++ Casks/pointiv.rb
@@ -1,7 +1,7 @@
 cask "pointiv" do
-  version "0.1.9"
-  sha256 "5b66e507133bc65236a4de239d2efbb02f2e1e9933ec6fce41bb2b91649f6463"
-  url "https://github.com/katkodeorg/pointiv/releases/download/v0.1.9/Pointiv_0.1.9_aarch64.dmg"
+  version "0.1.10"
+  sha256 "1f18a62a42582b3c2d89ec1bd7804c8e9da6e198d3df50e6ec240911338bde38"
+  url "https://github.com/katkodeorg/pointiv/releases/download/v0.1.10/Pointiv_0.1.10_aarch64.dmg"
   name "Pointiv"
   desc "Give your cursor agentic AI capabilities on macOS"
   homepage "https://github.com/katkodeorg/pointiv"
